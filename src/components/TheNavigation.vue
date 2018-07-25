8e41fb60cfb3966b5f1891e8b84be9c757f2c548
tu4mo <tu4mo@users.noreply.github.com>
2018-07-25 15:25:35 +0300

    Don't render nav when not authenticated

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,5 +1,8 @@
 <template>
-  <nav class="navigation">
+  <nav
+    v-if="$store.state.auth.isAuthenticated"
+    class="navigation"
+  >
     <div class="navigation__items">
       <NavItem
         :to="{ name: 'home' }"
