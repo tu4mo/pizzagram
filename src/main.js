0adda504d8774b2c74e3c5aca974983aa2ec9fa6
tu4mo <tu4mo@users.noreply.github.com>
2018-11-09 16:37:03 +0200

    Add Sentry

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from "vue";
 import VueLazyload from "vue-lazyload";
+import * as Sentry from "@sentry/browser";
 
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+Sentry.init({
+  dsn: "https://7f76df6d0d9e4d4a84a7f3676a5d4e46@sentry.io/1319696",
+  integrations: [new Sentry.Integrations.Vue({ Vue })]
+});
 
 Vue.config.productionTip = false;
 
