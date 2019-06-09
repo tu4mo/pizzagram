0728f7b1400f35b9bcfa3e141edb11083f07eb67
tu4mo <tu4mo@users.noreply.github.com>
2019-06-09 13:47:01 +0300

    Get userId from post

--- functions/add-notification.js
+++ functions/add-notification.js
@@ -2,14 +2,19 @@ const NOTIFICATION_TYPE_LIKE = "LIKE";
 
 module.exports = (db, notificationType) => async snap => {
   if (notificationType === NOTIFICATION_TYPE_LIKE) {
-    const { postId, userId } = snap.data();
+    const { postId } = snap.data();
+
+    const post = await db
+      .collection("posts")
+      .doc(postId)
+      .get();
 
     await db.collection("notifications").add({
       createdAt: new Date(),
       postId,
       read: false,
       type: NOTIFICATION_TYPE_LIKE,
-      userId
+      userId: post.data().userId
     });
   }
 };
