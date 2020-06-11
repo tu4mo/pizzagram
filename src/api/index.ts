d9418e66a8c4c126f46e4bd453378fb362976b89
tu4mo <tu4mo@users.noreply.github.com>
2020-06-11 21:51:35 +0300

    Use non-IndexedDB version of Firestore

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
-import 'firebase/firestore'
+import 'firebase/firestore/memory'
 import 'firebase/storage'
 import { GeoFirestore } from 'geofirestore'
 
