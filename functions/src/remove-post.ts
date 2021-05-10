2e300f43db8e417d62645932a0e211bdb4950b76
tu4mo <tu4mo@users.noreply.github.com>
2021-05-10 19:07:27 +0300

    Update Prettier

--- functions/src/remove-post.ts
+++ functions/src/remove-post.ts
@@ -3,61 +3,60 @@ import { Storage } from '@google-cloud/storage'
 
 const storage = new Storage()
 
-export default (db: admin.firestore.Firestore) => async (
-  snap: admin.firestore.DocumentSnapshot
-) => {
-  const { id } = snap
-  const postData = snap.data()
+export default (db: admin.firestore.Firestore) =>
+  async (snap: admin.firestore.DocumentSnapshot) => {
+    const { id } = snap
+    const postData = snap.data()
 
-  if (!postData) {
-    return
+    if (!postData) {
+      return
+    }
+
+    const { userId } = postData
+    const likes = db.collection('likes')
+    const notifications = db.collection('notifications')
+
+    const bucket = storage.bucket('pizzagram-cc.appspot.com')
+    const photoFile = bucket.file(`posts/${id}.jpg`)
+    const thumbnailFile = bucket.file(`posts/${id}_t.jpg`)
+
+    const usersCollection = db.collection('users')
+
+    try {
+      await photoFile.delete()
+      await thumbnailFile.delete()
+
+      console.log(`${photoFile.name} and ${thumbnailFile.name} removed.`)
+
+      const deleteBatch = db.batch()
+
+      const likesSnapshot = await likes.where('postId', '==', id).get()
+      likesSnapshot.forEach((doc) => {
+        deleteBatch.delete(likes.doc(doc.id))
+      })
+
+      const notificationsSnapshot = await likes.where('postId', '==', id).get()
+      notificationsSnapshot.forEach((doc) => {
+        deleteBatch.delete(notifications.doc(doc.id))
+      })
+
+      await deleteBatch.commit()
+
+      console.log(
+        `Removed ${likesSnapshot.size} likes and ${notificationsSnapshot.size} notifications.`
+      )
+
+      const usersSnapshot = await usersCollection
+        .where('id', '==', userId)
+        .limit(1)
+        .get()
+
+      usersSnapshot.forEach(async (userRef) => {
+        const posts = userRef.data().posts - 1
+        await usersCollection.doc(userRef.id).update({ posts })
+        console.log(`Decreasing ${userRef.id}'s posts count to ${posts}`)
+      })
+    } catch (error) {
+      console.log(`Failed to completely remove post (${error}).`)
+    }
   }
-
-  const { userId } = postData
-  const likes = db.collection('likes')
-  const notifications = db.collection('notifications')
-
-  const bucket = storage.bucket('pizzagram-cc.appspot.com')
-  const photoFile = bucket.file(`posts/${id}.jpg`)
-  const thumbnailFile = bucket.file(`posts/${id}_t.jpg`)
-
-  const usersCollection = db.collection('users')
-
-  try {
-    await photoFile.delete()
-    await thumbnailFile.delete()
-
-    console.log(`${photoFile.name} and ${thumbnailFile.name} removed.`)
-
-    const deleteBatch = db.batch()
-
-    const likesSnapshot = await likes.where('postId', '==', id).get()
-    likesSnapshot.forEach((doc) => {
-      deleteBatch.delete(likes.doc(doc.id))
-    })
-
-    const notificationsSnapshot = await likes.where('postId', '==', id).get()
-    notificationsSnapshot.forEach((doc) => {
-      deleteBatch.delete(notifications.doc(doc.id))
-    })
-
-    await deleteBatch.commit()
-
-    console.log(
-      `Removed ${likesSnapshot.size} likes and ${notificationsSnapshot.size} notifications.`
-    )
-
-    const usersSnapshot = await usersCollection
-      .where('id', '==', userId)
-      .limit(1)
-      .get()
-
-    usersSnapshot.forEach(async (userRef) => {
-      const posts = userRef.data().posts - 1
-      await usersCollection.doc(userRef.id).update({ posts })
-      console.log(`Decreasing ${userRef.id}'s posts count to ${posts}`)
-    })
-  } catch (error) {
-    console.log(`Failed to completely remove post (${error}).`)
-  }
-}
--- functions/src/update-post.ts
+++ functions/src/update-post.ts
@@ -1,28 +1,27 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
-export default (db: admin.firestore.Firestore) => async (
-  change: functions.Change<FirebaseFirestore.DocumentSnapshot>
-) => {
-  const postData = change.after.data()
+export default (db: admin.firestore.Firestore) =>
+  async (change: functions.Change<FirebaseFirestore.DocumentSnapshot>) => {
+    const postData = change.after.data()
 
-  if (!postData) {
-    return
+    if (!postData) {
+      return
+    }
+
+    const { userId } = postData
+
+    const usersSnapshot = await db
+      .collection('users')
+      .where('id', '==', userId)
+      .limit(1)
+      .get()
+
+    usersSnapshot.forEach(async (doc) => {
+      const posts = doc.data().posts + 1 || 1
+
+      console.log(`Increasing ${doc.id}'s posts count to ${posts}`)
+
+      await db.collection('users').doc(doc.id).update({ posts })
+    })
   }
-
-  const { userId } = postData
-
-  const usersSnapshot = await db
-    .collection('users')
-    .where('id', '==', userId)
-    .limit(1)
-    .get()
-
-  usersSnapshot.forEach(async (doc) => {
-    const posts = doc.data().posts + 1 || 1
-
-    console.log(`Increasing ${doc.id}'s posts count to ${posts}`)
-
-    await db.collection('users').doc(doc.id).update({ posts })
-  })
-}
