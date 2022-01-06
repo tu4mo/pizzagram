6c3d1f28e1ee48a407784d09ae7d45e7be3e4fbf
tu4mo <tu4mo@users.noreply.github.com>
2022-01-06 19:30:23 +0200

    Revert Tensorflow update

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -134,9 +134,8 @@
 
         this.$nextTick(async () => {
           try {
-            await import('@tensorflow/tfjs')
-            // await import('@tensorflow/tfjs-backend-cpu')
-            // await import('@tensorflow/tfjs-backend-webgl')
+            await import('@tensorflow/tfjs-backend-cpu')
+            await import('@tensorflow/tfjs-backend-webgl')
 
             const cocoSsd = await import('@tensorflow-models/coco-ssd')
             const model = await cocoSsd.load()
