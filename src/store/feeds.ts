e7e153580d4ec7d779c9ceff544c2d111f62677b
tu4mo <tu4mo@users.noreply.github.com>
2022-07-29 15:20:45 +0300

    Fix types

--- src/store/feeds.ts
+++ src/store/feeds.ts
@@ -1,6 +1,9 @@
 import { reactive } from 'vue'
 
-export const feedsStore = reactive({
+export const feedsStore = reactive<{
+  feeds: { [feed: string]: { [postId: string]: boolean } }
+  addToFeeds: (feed: string, postId: string) => void
+}>({
   feeds: {},
 
   addToFeeds(feed, postId) {
