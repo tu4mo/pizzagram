6ebac2665461b441f57e9b69e5ca22252df2ea1c
tu4mo <tu4mo@users.noreply.github.com>
2022-10-04 22:23:16 +0300

    Initialize auth callback early

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { BrowserTracing } from '@sentry/tracing'
 
 import App from './App.vue'
 import { router } from './router'
+import { initializeAuthCallback } from './store/auth'
+
+initializeAuthCallback()
 
 const app = createApp(App)
 
