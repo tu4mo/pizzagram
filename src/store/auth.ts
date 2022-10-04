c16c11eb207834efef670b002d326acfe8462eb3
tu4mo <tu4mo@users.noreply.github.com>
2022-10-04 22:23:16 +0300

    Initialize auth callback early

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -23,42 +23,45 @@ export const getIsMe = (userId: string | undefined) =>
 let unsubscribeToPosts: Unsubscribe = () => undefined
 let unsubscribeToNotifications: Unsubscribe | undefined = () => undefined
 
-setOnAuthStateChangedCallback(async (user) => {
-  if (user) {
-    try {
-      const userData = await fetchUser(user.uid)
+export const initializeAuthCallback = () =>
+  setOnAuthStateChangedCallback(async (user) => {
+    if (user) {
+      try {
+        const userData = await fetchUser(user.uid)
 
-      if (!userData) {
-        await signOut()
-        return
-      }
+        if (!userData) {
+          await signOut()
+          return
+        }
 
-      authStore.isAuthenticated = true
-      authStore.isInitialized = true
-      authStore.username = userData.username
-      authStore.userId = user.uid
+        authStore.isAuthenticated = true
+        authStore.isInitialized = true
+        authStore.username = userData.username
+        authStore.userId = user.uid
 
-      unsubscribeToPosts = subscribeToPosts((posts) => {
-        posts.forEach((post) => {
-          postsStore.posts[post.id] = post
-          addToFeed('home', post.id)
+        unsubscribeToPosts = subscribeToPosts((posts) => {
+          posts.forEach((post) => {
+            postsStore.posts[post.id] = post
+            addToFeed('home', post.id)
+          })
         })
-      })
 
-      unsubscribeToNotifications = subscribeToNotifications((notifications) => {
-        notificationsStore.notifications = notifications
-      })
-    } catch (e) {
-      // Sign out if there's an error getting user
-      await signOut()
+        unsubscribeToNotifications = subscribeToNotifications(
+          (notifications) => {
+            notificationsStore.notifications = notifications
+          }
+        )
+      } catch (e) {
+        // Sign out if there's an error getting user
+        await signOut()
+      }
+    } else {
+      unsubscribeToPosts?.()
+      unsubscribeToNotifications?.()
+
+      authStore.isAuthenticated = false
+      authStore.isInitialized = true
+      authStore.username = ''
+      authStore.userId = ''
     }
-  } else {
-    unsubscribeToPosts?.()
-    unsubscribeToNotifications?.()
-
-    authStore.isAuthenticated = false
-    authStore.isInitialized = true
-    authStore.username = ''
-    authStore.userId = ''
-  }
-})
+  })
