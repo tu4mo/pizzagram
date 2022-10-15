bc4b8d46b499eb772e7af46c221d86952f19febf
tu4mo <tu4mo@users.noreply.github.com>
2022-10-15 20:14:05 +0300

    Convert to CSS

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -79,73 +79,73 @@
   }
 </script>
 
-<style lang="scss" scoped>
-  .profile {
-    &__header {
-      background-attachment: fixed;
-      background-position: 50% 50%;
-      background-size: cover;
-      max-height: 16rem;
-      position: relative;
+<style scoped>
+  .profile__header {
+    background-attachment: fixed;
+    background-position: 50% 50%;
+    background-size: cover;
+    max-height: 16rem;
+    position: relative;
+  }
 
-      &::after {
-        content: '';
-        display: block;
-        padding-top: 50%;
-        pointer-events: none;
-      }
+  .profile__header::after {
+    content: '';
+    display: block;
+    padding-top: 50%;
+    pointer-events: none;
+  }
 
-      &::before {
-        background-image: linear-gradient(
-          to bottom,
-          rgba(var(--color-background-rgb), 0.75),
-          var(--color-background)
-        );
-        bottom: 0;
-        content: '';
-        left: 0;
-        position: absolute;
-        right: 0;
-        top: 0;
-      }
+  .profile__header::before {
+    background-image: linear-gradient(
+      to bottom,
+      rgba(var(--color-background-rgb), 0.75),
+      var(--color-background)
+    );
+    bottom: 0;
+    content: '';
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+
+  .profile__user {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin-top: -4rem;
+    padding: 0 2rem;
+    position: relative;
+  }
+
+  .profile__photo {
+    box-shadow: 0 0 4rem 2rem #fff;
+    margin-bottom: 0.5rem;
+  }
+
+  .profile__posts {
+    display: grid;
+    gap: 1px;
+    grid-template-columns: repeat(3, 1fr);
+    margin: 0 auto;
+    margin-top: 2rem;
+    list-style: none;
+  }
+
+  .profile__footer {
+    margin: 1rem 0;
+  }
+
+  @media (min-width: 640px) {
+    .profile__user {
+      margin-top: -8rem;
     }
 
-    &__user {
-      align-items: center;
-      display: flex;
-      flex-direction: column;
-      margin-top: -4rem;
-      padding: 0 2rem;
-      position: relative;
-
-      @media (min-width: 640px) {
-        margin-top: -8rem;
-      }
-    }
-
-    &__photo {
-      box-shadow: 0 0 4rem 2rem #fff;
-      margin-bottom: 0.5rem;
-    }
-
-    &__posts {
-      display: grid;
-      gap: 1px;
-      grid-template-columns: repeat(3, 1fr);
-      margin: 0 auto;
-      margin-top: 2rem;
-      list-style: none;
-
-      @media (min-width: 640px) {
-        gap: 1rem;
-        grid-template-columns: repeat(4, 1fr);
-        max-width: var(--content-width-lg);
-        padding: 2rem;
-      }
-    }
-
-    &__footer {
-      margin: 1rem 0;
+    .profile__posts {
+      gap: 1rem;
+      grid-template-columns: repeat(4, 1fr);
+      max-width: var(--content-width-lg);
+      padding: 2rem;
     }
   }
 </style>
