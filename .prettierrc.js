f6c6a67613d33d4bc01ee06542e0f44606363c47
tu4mo <tu4mo@users.noreply.github.com>
2023-10-14 18:09:08 +0300

    Format index.html

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,16 @@
+/** @type {import("prettier").Config} */
 module.exports = {
   htmlWhitespaceSensitivity: 'ignore',
   semi: false,
   singleQuote: true,
   vueIndentScriptAndStyle: true,
+
+  overrides: [
+    {
+      files: ['index.html'],
+      options: {
+        printWidth: 200,
+      },
+    },
+  ],
 }
