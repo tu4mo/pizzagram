8b097d5da197215203a8a88c5e126ead729cc942
tu4mo <tu4mo@users.noreply.github.com>
2023-10-15 14:11:52 +0300

    Fix function

--- functions/src/remove-comment-from-post.ts
+++ functions/src/remove-comment-from-post.ts
@@ -5,7 +5,7 @@ import type {
 } from 'firebase-functions/v2/firestore'
 
 type Comment = {
-  userId: string
+  id: string
 }
 
 export async function removeCommentFromPost(
@@ -19,20 +19,22 @@ export async function removeCommentFromPost(
   }
 
   const comment = snap.data()
+  const commentId = snap.id
   const post = await db.collection('posts').doc(comment.postId).get()
-  const postComments = ((post.get('comments') || []) as Comment[]).filter(
-    (postComment) => postComment.userId === comment.userId,
-  )
 
-  console.log(`Removing user ${comment.userId} comments from post ${post.id}`)
+  console.log(`Removing comment ${commentId} from post ${post.id}`)
 
   if (!post.exists) {
     console.log('Post does not exist')
     return
   }
 
-  if (postComments.length === 0) {
-    console.log('No comments to remove from post')
+  const postComment = ((post.get('comments') || []) as Comment[]).find(
+    (postComment) => postComment.id === commentId,
+  )
+
+  if (postComment) {
+    console.log('No comment to remove from post')
     return
   }
 
@@ -40,8 +42,8 @@ export async function removeCommentFromPost(
     .collection('posts')
     .doc(post.id)
     .update({
-      comments: admin.firestore.FieldValue.arrayRemove(postComments),
+      comments: admin.firestore.FieldValue.arrayRemove(postComment),
     })
 
-  console.log(`Comments removed`)
+  console.log(`Comment removed`)
 }
