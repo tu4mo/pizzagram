e10ae5fa2a2e0f01cdf839a1fc73fdf0f9ec7cd2
tu4mo <tu4mo@users.noreply.github.com>
2024-02-22 10:23:30 +0200

    Update deprecated method

--- src/sentry.ts
+++ src/sentry.ts
@@ -8,8 +8,8 @@ export function initializeSentry(app: Vue, router: Router) {
       app,
       dsn: 'https://7f76df6d0d9e4d4a84a7f3676a5d4e46@sentry.io/1319696',
       integrations: [
-        new Sentry.BrowserTracing({
-          routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+        Sentry.browserTracingIntegration({
+          router,
           tracingOrigins: ['localhost', 'pizzagram.cc', /^\//],
         }),
       ],
