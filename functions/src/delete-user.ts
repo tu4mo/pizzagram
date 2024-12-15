d3c2a8cd9ce5de27e3dbba3918927947b700e06e
tu4mo <tu4mo@users.noreply.github.com>
2024-12-15 14:29:54 +0200

    Improve delete-user

--- functions/src/delete-user.ts
+++ functions/src/delete-user.ts
@@ -1,4 +1,5 @@
 import * as admin from 'firebase-admin'
+import { Filter } from 'firebase-admin/firestore'
 import type * as functionsV1 from 'firebase-functions/v1'
 
 import { comments, db, feeds, likes, notifications, posts, users } from './db'
@@ -7,64 +8,61 @@ export async function deleteUser(user: functionsV1.auth.UserRecord) {
   const { uid, email } = user
   const deleteBatch = db.batch()
 
-  console.log(`Removing ${email} (${uid})...`)
+  console.group(`Deleting ${email} (${uid})...`)
 
-  // Remove comments
   const commentsData = await comments.where('userId', '==', uid).get()
   commentsData.forEach((doc) => {
+    console.log('Deleting comment', doc.id)
     deleteBatch.delete(comments.doc(doc.id))
   })
 
-  // Remove likes
   const likesData = await likes.where('userId', '==', uid).get()
   likesData.forEach((doc) => {
+    console.log('Deleting like', doc.id)
     deleteBatch.delete(likes.doc(doc.id))
   })
 
-  // Remove likes from posts
   const postsWithLikes = await posts.where(`likes.${uid}`, '==', true).get()
   postsWithLikes.forEach((doc) => {
+    console.log('Deleting like from post', doc.id)
     deleteBatch.update(posts.doc(doc.id), {
       [`likes.${uid}`]: admin.firestore.FieldValue.delete(),
     })
   })
 
-  // Remove notifications to user
-  const notificationsToUser = await notifications
-    .where('userId', '==', uid)
+  const notificationsData = await notifications
+    .where(
+      Filter.or(
+        Filter.where('userId', '==', uid),
+        Filter.where('fromUserId', '==', uid),
+      ),
+    )
     .get()
-  notificationsToUser.forEach((doc) => {
-    deleteBatch.delete(notifications.doc(doc.id))
-  })
-  const notificationsFromUser = await notifications
-    .where('fromUserId', '==', uid)
-    .get()
-  notificationsFromUser.forEach((doc) => {
+  notificationsData.forEach((doc) => {
+    console.log('Deleting notification', doc.id)
     deleteBatch.delete(notifications.doc(doc.id))
   })
 
-  // Remove posts
   const postsData = await posts.where('userId', '==', uid).get()
   postsData.forEach((doc) => {
+    console.log('Deleting post', doc.id)
     deleteBatch.delete(posts.doc(doc.id))
   })
 
-  // Remove feed
+  console.log('Deleting feed', uid)
   deleteBatch.delete(feeds.doc(uid))
 
-  // Remove user
+  console.log('Deleting user', uid)
   deleteBatch.delete(users.doc(uid))
 
   await deleteBatch.commit()
 
   console.log(
-    `Removed ${commentsData.size} comments, ` +
-      `${likesData.size} likes, ` +
-      `${
-        notificationsToUser.size + notificationsFromUser.size
-      } notifications, ` +
-      `${postsData.size} posts.`,
+    `Deleted ${commentsData.size} comments,`,
+    `${likesData.size} likes,`,
+    `${postsWithLikes.size} likes from posts,`,
+    `${notificationsData.size} notifications,`,
+    `${postsData.size} posts.`,
   )
-  console.log(`Removed feed ${uid}.`)
-  console.log(`Removed user ${uid}.`)
+  console.groupEnd()
 }
