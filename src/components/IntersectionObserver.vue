b461e59dda895b0ecbc3bf514a65b022612f3d30
tu4mo <tu4mo@users.noreply.github.com>
2025-07-13 21:12:24 +0300

    Optimize

--- src/components/IntersectionObserver.vue
+++ src/components/IntersectionObserver.vue
@@ -2,7 +2,6 @@
   import { onMounted, onUnmounted, ref, useTemplateRef } from 'vue'
 
   const { enabled } = defineProps<{ enabled: boolean }>()
-
   const emit = defineEmits<(event: 'is-intersecting') => void>()
 
   const div = useTemplateRef('div')
@@ -12,18 +11,19 @@
     if (!div.value) return
 
     observer.value = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
+      for (const entry of entries) {
         if (entry.isIntersecting && enabled) {
           emit('is-intersecting')
+          break
         }
-      })
+      }
     })
 
     observer.value.observe(div.value)
   })
 
   onUnmounted(() => {
-    if (observer.value) observer.value.disconnect()
+    observer.value?.disconnect()
   })
 </script>
 
